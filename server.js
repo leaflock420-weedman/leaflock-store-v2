dc9be2721242dd2042436348b8e3b1e86361a3f5
wordo <wordo@users.noreply.github.com>
2026-06-23 22:39:21 +1000

    Fix root URL: rewrite / to index.html on Node server

--- server.js
+++ server.js
@@ -4,14 +4,17 @@ const handler = require("serve-handler");
 const port = Number(process.env.PORT) || 10000;
 const host = "0.0.0.0";
 
-const server = createServer((req, res) =>
+const server = createServer((req, res) => {
+  if (req.url === "/" || req.url === "") {
+    req.url = "/index.html";
+  }
   handler(req, res, {
     public: ".",
     cleanUrls: false,
     trailingSlash: false,
     directoryListing: false,
-  })
-);
+  });
+});
 
 server.listen(port, host, () => {
   console.log(`LeafLock Store running at http://${host}:${port}`);
